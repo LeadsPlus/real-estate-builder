5aa4e21fde6d96efb55af17b8c21d46006125fbb
Matt Barba <matt@placester.com>
2012-02-09 09:31:44 -0500

    The new skinnier, easier to maintain, less insane plugin.
    Signed-off-by: Matt Barba <matt@placester.com>

--- options/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ( !function_exists( 'placester_of_init' ) ) {
-    define( 'PL_OPTIONS_FRAMEWORK_DIRECTORY', plugins_url( '', __FILE__ ) . '/' );
-    define( 'PL_OPTIONS_FRAMEWORK_URL', dirname( __FILE__) );
-    require_once ( PL_OPTIONS_FRAMEWORK_URL . '/core.php');
-}
