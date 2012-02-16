0ee30cd2a8fe8660625f8b5063fda26901e66e94
Matt Barba <matt@placester.com>
2012-02-16 11:45:08 -0500

    Check to make sure there's actually a valid response, if not, return empty so dependent libraries don't crash.
    Signed-off-by: Matt Barba <matt@placester.com>

--- models/listing.php
+++ models/listing.php
@@ -3,14 +3,18 @@
 class PL_Listing {
 	
 	public function get($args = array()) {
-		$config = PL_Config::PL_API_LISTINGS('get');
-		$request = array_merge(array("api_key" => PL_Option_Helper::api_key()), PL_Validate::request($args, $config['args']));
-		$response = PL_HTTP::send_request($config['request']['url'], $request, $config['request']['type']);
-		if (isset($response) && isset($response['listings'])) {
+		// merge incoming args with preset options, basically api key at this point.
+		$request = array_merge(array("api_key" => PL_Option_Helper::api_key()), PL_Validate::request($args, PL_Config::PL_API_LISTINGS('get', 'args')));
+		// sent request, use details from config.
+		$response = PL_HTTP::send_request(PL_Config::PL_API_LISTINGS('get', 'request', 'url'), $request, PL_Config::PL_API_LISTINGS('get', 'request', 'type'));
+		// validate response. 
+		if (isset($response) && isset($response['listings']) && is_array($response['listings'])) {
 			foreach ($response['listings'] as $key => $listing) {
-				$response['listings'][$key] = PL_Validate::attributes($listing, $config['returns']);
+				$response['listings'][$key] = PL_Validate::attributes($listing, PL_Config::PL_API_LISTINGS('get','returns'));
 			}
-		}	
+		} else {
+			$response = PL_Validate::attributes($response, array('listings' => array(), 'total' => 0));
+		}
 		return $response;
 	}
 
