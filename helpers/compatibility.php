68c329a75893683e7da4321c702efa58ef7f8fec
Matt Barba <matt@placester.com>
2012-02-23 18:34:23 -0500

    Compatibility updates to get blueprint to even load.
    Signed-off-by: Matt Barba <matt@placester.com>

--- helpers/compatibility.php
+++ helpers/compatibility.php
@@ -2,3 +2,13 @@
 
 function placester_get_api_key () { return PL_Option_Helper::api_key(); }
 function placester_post_slug () { return PL_Option_Helper::post_slug(); }
+function placester_register_filter_form() {return;}
+function placester_location_list () {
+	$locations->city = array();
+	$locations->state = array();
+	$locations->zip = array();
+ 	return $locations;
+}
+function placester_listings_list () {}
+class Placester_Contact_Widget { function widget() {}}
+function placester_get_user_details() {}
