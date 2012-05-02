b7f615bb69eb2595a4104d76fe2f198eccd4ad30
Matt Barba <matt@placester.com>
2012-05-02 09:24:50 -0400

    update to pages library to properly classify listings into taxonomies.
    Signed-off-by: Matt Barba <matt@placester.com>

--- lib/pages.php
+++ lib/pages.php
@@ -6,11 +6,12 @@ class PL_Pages {
 	static $property_post_type = 'property';
 
 	function init() {
+		add_action('admin_init', 'flush_rewrite_rules');
 		add_action('init', array(__CLASS__, 'create_taxonomies'));
 		add_action('wp_footer', array(__CLASS__,'force_rewrite_update'));
 		add_action('admin_footer', array(__CLASS__,'force_rewrite_update'));
+		add_filter('post_type_link', array(__CLASS__, 'get_property_permalink'), 20, 3);
 	}
-
 
 	//return many page urls
 	function get () {
@@ -40,7 +41,17 @@ class PL_Pages {
 		$page_details['title'] = $api_listing['location']['address'];
 		$page_details['name'] = $api_listing['id'];
 		$page_details['content'] = serialize($api_listing);
-		$page_details['taxonomies'] = array('zip_code' => $api_listing['location']['postal']);
+		$page_details['taxonomies'] = array(
+										'zip' => $api_listing['location']['postal'], 
+										'city' => $api_listing['location']['locality'],
+										'state' => $api_listing['location']['region'],
+										'neighborhood' => $api_listing['location']['neighborhood'],
+										'street' => $api_listing['location']['address'],
+										'beds' => (string) $api_listing['cur_data']['beds'],
+										'baths' => (string) $api_listing['cur_data']['beds'],
+										'half-baths' => (string) $api_listing['cur_data']['half_baths']
+									);
+		// pls_dump($page_details['taxonomies']);
 		return self::manage($page_details);
 	}
 
@@ -106,11 +117,85 @@ class PL_Pages {
     	return false;
 	}
 
+	function get_property_permalink ($permalink, $post_id, $leavename) {
+		$post = get_post($post_id);
+		$state = '';
+		$zip = '';
+		$city = '';
+		$street = '';
+        $rewritecode = array('%state%','%city%','%zip%','%neighborhood%','%street%', $leavename ? '' : '%postname%', $leavename ? '' : '%pagename%', $leavename ? '' : '%pagename%');
+        
+        if ( !empty($permalink) && $post->post_type == 'property' && !in_array($post->post_status, array('draft', 'pending', 'auto-draft')) ) {
+        
+            if (strpos($permalink, '%state%')) {
+            	$terms = wp_get_object_terms($post->ID, 'state');  
+            	if (!is_wp_error($terms) && !empty($terms) && is_object($terms[0])) {
+            		$state = $terms[0]->slug;	
+            	} else {
+            		$state = 'unassigned-state';
+            	}
+            }
+
+            if (strpos($permalink, '%zip%')) {
+            	$terms = wp_get_object_terms($post->ID, 'zip');  
+            	if (!is_wp_error($terms) && !empty($terms) && is_object($terms[0])) {
+            		$zip = $terms[0]->slug;	
+            	} else {
+            		$zip = 'unassigned-zip';
+            	}
+            }
+
+	        if (strpos($permalink, '%city%')){
+	            $terms = wp_get_object_terms($post->ID, 'city');  
+	            if (!is_wp_error($terms) && !empty($terms) && is_object($terms[0])) {
+	            	$city = $terms[0]->slug;	
+	            } else {
+	            	$city = 'unassigned-city';
+	            } 
+	        } 
+
+	        if (strpos($permalink, '%neighborhood%')){
+	            $terms = wp_get_object_terms($post->ID, 'neighborhood');  
+	            if (!is_wp_error($terms) && !empty($terms) && is_object($terms[0])) {
+	            	$neighborhood = $terms[0]->slug;	
+	            } else {
+	            	$neighborhood = 'unassigned-neighborhood';
+	            } 
+	        } 
+
+	        if (strpos($permalink, '%street%')){
+	            $terms = wp_get_object_terms($post->ID, 'street');  
+	            if (!is_wp_error($terms) && !empty($terms) && is_object($terms[0])) {
+	            	$street = $terms[0]->slug;	
+	            } else {
+	            	$street = 'unassigned-street';
+	            } 
+	        }           
+
+	        $rewritereplace = array( $state, $city, $zip, $neighborhood, $street, $post->post_name, $post->post_name, $post->post_name, $post->post_name, $post->post_name);
+	        $permalink = str_replace($rewritecode, $rewritereplace, $permalink);
+        }
+        return $permalink;
+	}
+
 	function create_taxonomies() {
-			// register_taxonomy('zip_code', self::$property_post_type, array('hierarchical' => TRUE,'label' => __('Zip Codes'), 'public' => TRUE,'show_ui' => TRUE,'query_var' => true,'rewrite' => array('slug' => 'properties/zip', 'with_front' => false) ) );
-			register_post_type(self::$property_post_type, array('labels' => array('name' => __( 'Properties' ),'singular_name' => __( 'property' )),'public' => true,'has_archive' => true, 'rewrite' => array('slug' => 'properties', 'with_front' => false)));
-			// register_post_type('client', array('labels' => array('name' => __( 'client' ),'singular_name' => __( 'client' )),'public' => true,'has_archive' => true, 'rewrite' => array('slug' => 'client', 'with_front' => false)));
-			// register_post_type('search', array('labels' => array('name' => __( 'search' ),'singular_name' => __( 'search' )),'public' => true,'has_archive' => true, 'rewrite' => array('slug' => 'search', 'with_front' => false)));
+		register_post_type(self::$property_post_type, array('labels' => array('name' => __( 'Properties' ),'singular_name' => __( 'property' )),'public' => true,'has_archive' => true, 'rewrite' => true, 'query_var' => true));
+
+		register_taxonomy('state', 'property', array('hierarchical' => TRUE,'label' => __('States'), 'public' => TRUE,'show_ui' => TRUE,'query_var' => true,'rewrite' => array('with_front' => false, 'hierarchical' => false) ) );
+		register_taxonomy('zip', 'property', array('hierarchical' => TRUE,'label' => __('Zip Codes'), 'public' => TRUE,'show_ui' => TRUE,'query_var' => true,'rewrite' => array('with_front' => false, 'hierarchical' => false) ) );
+		register_taxonomy('city', 'property', array('hierarchical' => TRUE,'label' => __('Cities'), 'public' => TRUE,'show_ui' => TRUE,'query_var' => true,'rewrite' => array('with_front' => false, 'hierarchical' => false) ) );
+		register_taxonomy('neighborhood', 'property', array('hierarchical' => TRUE,'label' => __('Neighborhoods'), 'public' => TRUE,'show_ui' => TRUE,'query_var' => true,'rewrite' => array('with_front' => false, 'hierarchical' => false) ) );
+		register_taxonomy('street', 'property', array('hierarchical' => TRUE,'label' => __('Streets'), 'public' => TRUE,'show_ui' => TRUE,'query_var' => true,'rewrite' => array('with_front' => false, 'hierarchical' => false) ) );
+		
+		register_taxonomy('beds', 'property', array('hierarchical' => TRUE,'label' => __('Beds'), 'public' => TRUE,'show_ui' => TRUE,'query_var' => true,'rewrite' => true ) );
+		register_taxonomy('baths', 'property', array('hierarchical' => TRUE,'label' => __('Baths'), 'public' => TRUE,'show_ui' => TRUE,'query_var' => true,'rewrite' => true ) );
+		register_taxonomy('half-baths', 'property', array('hierarchical' => TRUE,'label' => __('Half-baths'), 'public' => TRUE,'show_ui' => TRUE,'query_var' => true,'rewrite' => true ) );
+		
+
+		global $wp_rewrite;
+	    $property_structure = '/property/%state%/%city%/%zip%/%neighborhood%/%street%/%'.self::$property_post_type.'%';
+        $wp_rewrite->add_rewrite_tag("%property%", '([^/]+)', "property=");
+        $wp_rewrite->add_permastruct('property', $property_structure, false);
 	}
 
 	function force_rewrite_update () {
