84dab7d13a8a34c6f1d5d3f2b437795b83b91cb1
Matt Barba <matt@placester.com>
2012-05-16 14:17:16 -0400

    add theme reporting on switch
    Signed-off-by: Matt Barba <matt@placester.com>

--- helpers/wordpress.php
+++ helpers/wordpress.php
@@ -1,7 +1,17 @@
 <?php 
 
+PL_WordPress_Helper::init();
 class PL_WordPress_Helper {
+	
+	function init () {
+		add_action('switch_theme', array(__CLASS__, 'report_theme'));
+	}
 
+	function report_theme () {
+		$theme = strtolower(get_current_theme());
+		$response = PL_WordPress::set(array_merge(array('theme' => $theme), array('url' => site_url() ) ) );
+		return $response;
+	}
 
 	function report_url () {
 		$request = array('url' => site_url());
