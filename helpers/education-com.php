fb10703310ae7fce813f7ffb1d0843efb22655e5
Matt Barba <matt@placester.com>
2012-06-20 09:45:27 -0400

    update to education.com helper to support school search via city+state or lat/lng.
    Signed-off-by: Matt Barba <matt@placester.com>

--- helpers/education-com.php
+++ helpers/education-com.php
@@ -3,6 +3,28 @@
 class PL_Education_Helper {
 
 	function get_schools ($args = array()) {
+		$args = wp_parse_args($args, array('edu_key' => false, 'area_search' =>  false, 'lat_lng_search' => false, 'search_params' => '', 'maxResult' => 10 ) );
+		if ($args['area_search']) {
+			if (isset($args['area_search']['zip'] ) )  {
+				$args['search_params'] .= '&zip=' . $args['area_search']['zip'];
+			}
+			if (isset($args['area_search']['city']) ) {
+				$args['search_params'] .= '&city=' . urlencode($args['area_search']['city']);
+			}
+			if (isset($args['area_search']['county']) ) {
+				$args['search_params'] .= '&county=' . $args['area_search']['county'];
+			}
+			if (isset($args['area_search']['state']) ) {
+				$args['search_params'] .= '&state=' . $args['area_search']['state'];
+			}
+			if (isset($args['area_search']['distance']) ) {
+				$args['search_params'] .= '&distance=' . $args['area_search']['distance'];
+			}
+		} elseif ($args['lat_lng_search'] && isset($args['lat_lng_search']['latitude']) && isset($args['lat_lng_search']['longitude']) && $args['lat_lng_search']['distance'] ) {
+			$args['search_params'] .= '&latitude=' . $args['lat_lng_search']['latitude'];
+			$args['search_params'] .= '&longitude=' . $args['lat_lng_search']['longitude'];
+			$args['search_params'] .= '&distance=' . $args['lat_lng_search']['distance'];
+		}
 		$response = PL_Education::search($args);
 		if ($response) {
 			return $response;
--- models/education-com.php
+++ models/education-com.php
@@ -5,9 +5,14 @@ class PL_Education {
 	function search ($args = array()) {
 		extract(wp_parse_args($args, array('edu_key' => false)));
 		if ($edu_key) {
-			$response = wp_remote_get('http://api.education.com/service/service.php?f=schoolSearch&sn=sf&v=4&resf=json&key=' . $edu_key , array('timeout' => 10));
+			$response = wp_remote_get('http://api.education.com/service/service.php?f=schoolSearch&sn=sf&v=4&resf=json&maxResult='.$maxResult.'&key=' . $edu_key . $search_params , array('timeout' => 10));
 			if (is_array($response) && isset($response['body']) ) {
-				return json_decode($response['body'], true);
+				$response = json_decode($response['body'], true);
+				if (isset($response['faultCode'])) {
+					return array();
+				} else {
+					return $response;
+				}
 			} else {
 				return array();
 			}
