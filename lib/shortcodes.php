9210495037fbb8329423a2679c98fca0ce2f76ae
iantendick <itendick@placester.com>
2012-08-13 02:46:46 -0400

    Worked through some issues

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -8,15 +8,15 @@
 PL_Shortcodes::init();
 class PL_Shortcodes
 {
-	public static $codes = array('searchform', 'prop_details', 'list');
+	public static $codes = array('searchform', 'listings', 'prop_details');
 
-	public static $p_codes = array('searchform' => 'Search Form', 'prop_details' => 'Property Details', 'list' => 'List');
+	public static $p_codes = array('searchform' => 'Search Form', 'prop_details' => 'Property Details', 'listings' => 'Listings');
 	
 	public static $defaults = array('searchform' 	=> array('ventura', 'columbus', 'highland'),
 	  				                'prop_details' 	=> array('Red', 'Yellow', 'Orange'),
-			               			'list' 			=> array('Purple', 'Pink', 'Gray') );
+			               			'listings' 		=> array('Purple', 'Pink', 'Gray') );
 
-	public static $subcodes = array('searchform'   => array('bedrooms',
+	public static $subcodes = array('searchform'  =>  array('bedrooms',
 												            'min_beds',
 												            'max_beds',
 												            'bathrooms',
@@ -37,15 +37,18 @@ class PL_Shortcodes
 												            'min_price',
 												            'max_price',
 												            'min_price_rental',
-            												'max_price_rental') );
+	        												'max_price_rental'),
+            						'listings'	  =>  array('')
+            						);
 
+	// TODO: These are a temporary solution, come up with a better convention...
 	public static $form_html = false;
+	public static $item_html = false;
 
 	public function init() 
 	{
 		add_shortcode('searchform', array(__CLASS__, 'searchform_shortcode_handler'));
-		add_shortcode('prop_details', array(__CLASS__, 'prop_details_shortcode_handler'));
-		add_shortcode('list', array(__CLASS__, 'list_shortcode_handler'));
+		add_shortcode('listings', array(__CLASS__, 'listings_shortcode_handler'));
 
 		// For any shortcodes that use subcodes, register them to a single handler that bears the shortcode's name
 		foreach (self::$subcodes as $code => $subs) {
@@ -55,7 +58,9 @@ class PL_Shortcodes
 		}
 
 		// Register hooks to generate the forms...
-		add_filter('pls_listings_search_form_outer_pls_shortcode', array(__CLASS__, 'searchform_shortcode_context'), 10, 6);
+		add_filter('pls_listings_search_form_outer_shortcode', array(__CLASS__, 'searchform_shortcode_context'), 10, 6);
+		add_filter('pls_listings_list_ajax_item_html_shortcode', array(__CLASS__, 'listings_shortcode_context'), 10, 3);
+		add_filter('pls_listing_property_details', array(__CLASS__, 'prop_details_shortcode_handler'), 10, 2);
 
 		// TODO: Construct defaults array properly...
 
@@ -65,12 +70,28 @@ class PL_Shortcodes
 		}
 	}
 
+
+/*** Shortcode Handlers ***/	
+	
 	public static function searchform_shortcode_handler($atts)
 	{
 		// Handle attributes using shortcode_atts...
-		
-		return PLS_Partials_Listing_Search_Form::init(array('context' => 'pls_shortcode'));
+		// Ajax setting as an attr?
+
+		return PLS_Partials_Listing_Search_Form::init(array('context' => 'shortcode', 'ajax' => true));
 	}
+
+	public static function listings_shortcode_handler($atts)
+	{
+		// Handle attributes using shortcode_atts...
+		// These attributes will hand the look and feel of the listing form container, as 
+		// the context func applies to each individual listing.
+	
+		return PLS_Partials_Get_Listings_Ajax::load(array('context' => 'shortcode'));
+	}
+
+
+/*** Context Filter Handlers ***/	
 
 	public static function searchform_shortcode_context($form, $form_html, $form_options, $section_title, $form_data)
 	{
@@ -78,6 +99,56 @@ class PL_Shortcodes
 		self::$form_html = $form_html;
 
 	  ob_start();
+		$snippet_body = self::get_active_snippet_body($shortcode);
+		echo do_shortcode($snippet_body);
+	  return ob_get_clean(); 
+	}
+
+	// It's important to note that this is called for every individual listing...
+	public static function listings_shortcode_context($item_html, $listing) 
+	{
+		ob_start();
+		  echo $item_html;
+		return ob_get_clean();
+
+		// $shortcode = 'listings';
+		// self::$form_html = $form_html;
+
+	 //  ob_start();
+	 //  	$snippet_body = get_active_snippet_body($shortcode);
+	 //  	echo do_shortcode($snippet_body);
+	 //  return ob_get_clean();
+	}
+
+	public static function prop_details_shortcode_context() 
+	{
+
+	}
+
+
+/*** Sub-Shortcode Handlers ***/
+
+	public static function searchform_sub_shortcode_handler ($atts, $content, $tag) 
+	{
+		//pls_dump($tag);
+		echo self::$form_html[$tag];
+	}
+
+	public static function listings_sub_shortcode_handler ($atts, $content, $tag) 
+	{
+
+	}
+
+	public static function prop_details_sub_shortcode_handler ($atts, $content, $tag)
+	{
+
+	}
+
+
+/*** Helper Functions ***/
+
+	private static function get_active_snippet_body($shortcode)
+	{
 		// Get snippet ID currently associated with this shortcode...
 		$option_key = ('pls_' . $shortcode);
 		$snippet_name = get_option($option_key, self::$defaults[$shortcode][0]);
@@ -86,34 +157,9 @@ class PL_Shortcodes
 		$snippet_DB_key = ('pls_' . $shortcode . '_' . $snippet_name);
 		$type = ( get_option($snippet_DB_key) ? 'custom' : 'default' );
 
-		$snippet_body = PL_Router::load_snippet($shortcode, $snippet_name, 'custom'); // define this properly...
-		echo do_shortcode($snippet_body);
-	  return ob_get_clean(); 
+		$snippet_body = PL_Router::load_snippet($shortcode, $snippet_name, $type);
+		return $snippet_body;
 	}
-
-	public static function property_details_shortcode_handler($atts)
-	{
-		// Handle attributes using shortcode_atts...
-		return '<h2>Property Details go here...</h2>';
-	}
-
-	public static function list_shortcode_handler($atts)
-	{
-		return PLS_Partials_Get_Listings_Ajax::load(array('context' => 'adfsasdfasdf'));
-		// Handle attributes using shortcode_atts...
-	}
-
-	/*** Subcode Handlers ***/
-
-	public static function searchform_sub_shortcode_handler ($atts, $content, $tag) {
-		pls_dump($tag);
-		echo self::$form_html[$tag];
-	}
-
-	public static function list_sub_shortcode_handler ($tag) {
-
-	}
-
 }
 
 ?>
