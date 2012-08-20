51dece45b5108e2bc88d09e09884dce588e773ac
Dave Ross <dave@davidmichaelross.com>
2012-08-20 14:52:33 -0400

    merging PLS_Cache & PL_Cache refactoring into plugin repo

--- lib/caching.php
+++ lib/caching.php
@@ -3,20 +3,47 @@
 PL_Cache::init();
 class PL_Cache {
 
+	const TTL_LOW  = 900; // 15 minutes
+	const TTL_HIGH = 172800; // 48 hours
+
+	public static $offset = 0;
 	public $type = 'general';
 	public $transient_id = false;
 
-	function __construct ($type) {
+	function __construct ($type = 'general') {
+		$this->offset = get_option('pls_cache_offset', 0);
 		$this->type = $type;
 	}
 
 	public static function init () {
+
+		// Allow cache to be cleared by going to url like http://example.com/?clear_cache
+		if(isset($_GET['clear_cache']) || isset($_POST['clear_cache'])) {
+			// style-util.php calls its PLS_Style::init() immediately
+			// so this can't be tied to a hook
+			self::invalidate();
+		}
+
 		add_action('wp_ajax_user_empty_cache', array(__CLASS__, 'clear' ) );
+		add_action('switch_theme', array(__CLASS__, 'invalidate'));
+
 	}
 
 	function get () {
-		$arg_hash = md5(http_build_query(func_get_args()));
-		$this->transient_id = 'pl_' . $this->type . '_' . $arg_hash;
+
+		// Just ignore caching for admins
+		if(is_admin() || is_admin_bar_showing()) {
+			return false;
+		}
+
+		// Backdoor to ignore the cache completely
+		if(isset($_GET['no_cache']) || isset($_POST['no_cache'])) {
+			return false;
+		}
+	
+		$func_args = func_get_args();
+		$arg_hash = rawToShortMD5(MD5_85_ALPHABET, md5(http_build_query( $func_args ), true));
+		$this->transient_id = 'pl_' . $this->type . $this->offset . '_' . $arg_hash;
         $transient = get_transient($this->transient_id);
         if ($transient) {
         	return $transient;
@@ -26,7 +53,9 @@ class PL_Cache {
 	}
 
 	public function save ($result, $duration = 172800) {
-		if ($this->transient_id) {
+		// Don't save any content from logged in users
+		// We were getting things like "log out" links cached
+		if ($this->transient_id && !is_user_logged_in()) {
 			set_transient($this->transient_id, $result , $duration);
 		}
 	}
@@ -57,5 +86,58 @@ class PL_Cache {
 		return $result;
 	}
 
+	public static function invalidate() {
+		$cache = new self();
+		$cache->offset += 1;
+		if($cache->offset > 99) {
+			$cache->offset = 0;
+		}
+		update_option('pls_cache_offset', $cache->offset);
+	}
+
 //end class
+}
+
+// Flush our cache when admins save option pages or configure widgets
+add_action('init', 'PL_Options_Save_Flush');
+function PL_Options_Save_Flush() {
+	// Check if options are being saved
+	$doing_ajax = ( defined('DOING_AJAX') && DOING_AJAX );
+	$editing_widgets = ( isset($_GET['savewidgets']) || isset($_POST['savewidgets']));
+	if($_SERVER['REQUEST_METHOD'] == 'POST' && is_admin() && (!$doing_ajax || $editing_widgets)) {
+
+		// Flush the cache
+		PL_Cache::invalidate();
+
+	}
+}
+
+/* Functions for converting between notations and short MD5 generation.
+ * No license (public domain) but backlink is always welcome :)
+ * By Proger_XP. http://proger.i-forge.net/Short_MD5
+ * Usage: rawToShortMD5(MD5_85_ALPHABET, md5($str, true))
+ * (passing true as the 2nd param to md5 returns raw binary, not a hex-encoded 32-char string)
+ */
+define('MD5_24_ALPHABET', '0123456789abcdefghijklmnopqrstuvwxyzABCDE');
+define('MD5_85_ALPHABET', '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()"|;:?\/\'[]<>');
+
+function RawToShortMD5($alphabet, $raw) {
+  $result = '';
+  $length = strlen(DecToBase($alphabet, 2147483647));
+
+  foreach (str_split($raw, 4) as $dword) {
+    $dword = ord($dword[0]) + ord($dword[1]) * 256 + ord($dword[2]) * 65536 + ord($dword[3]) * 16777216;
+    $result .= str_pad(DecToBase($alphabet, $dword), $length, $alphabet[0], STR_PAD_LEFT);
+  }
+
+  return $result;
+}
+
+function DecToBase(&$alphabet, $dword) {
+  $rem = fmod($dword, strlen($alphabet));
+  if ($dword < strlen($alphabet)) {
+    return $alphabet[$rem];
+  } else {
+    return DecToBase($alphabet, ($dword - $rem) / strlen($alphabet)).$alphabet[$rem];
+  }
 }
