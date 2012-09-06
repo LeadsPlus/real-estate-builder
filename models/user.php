d2ed83ed346e46d2a5f1a7eb3e1bf64482a92c39
Matt Barba <matt@placester.com>
2012-09-06 12:37:40 -0400

    allow whoami requests to be cached
    Signed-off-by: Matt Barba <matt@placester.com>

--- models/user.php
+++ models/user.php
@@ -10,7 +10,7 @@ class PL_User {
 		}
 
 		$request = array_merge(array("api_key" => PL_Option_Helper::api_key()), PL_Validate::request($args, PL_Config::PL_API_USERS('whoami', 'args')));
-		$response = PL_HTTP::send_request(trailingslashit(PL_Config::PL_API_USERS('whoami', 'request', 'url')), $request, PL_Config::PL_API_USERS('whoami', 'request', 'type'), false);
+		$response = PL_HTTP::send_request(trailingslashit(PL_Config::PL_API_USERS('whoami', 'request', 'url')), $request, PL_Config::PL_API_USERS('whoami', 'request', 'type'), true);
 		$response = PL_Validate::attributes($response, PL_Config::PL_API_USERS('whoami', 'returns'));
 		$memo[$arg_str] = $response;
 		return $response;
